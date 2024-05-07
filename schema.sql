df89f59d747c12697cf62b7b252c2e77c4e20b4c
Ameer Arsala <ameer.arsala03@gmail.com>
2024-05-06 22:46:05 -0700

    added db schema

---
+++ schema.sql
@@ -0,0 +1,40 @@
+-- Drop existing tables if they exist
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS chunks;
+DROP TABLE IF EXISTS communities;
+DROP TABLE IF EXISTS lore;
+DROP TABLE IF EXISTS belongings;
+
+-- Create tables
+
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  email TEXT UNIQUE NOT NULL,
+  chunk_name INTEGER REFERENCES chunks(name)
+);
+
+CREATE TABLE chunks(
+  name TEXT PRIMARY KEY,
+  profile TEXT,
+  community_id INTEGER REFERENCES communities(id),
+  parent_chunk TEXT REFERENCES chunks(name)
+);
+
+CREATE TABLE communities(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+-- "Knowledge"
+CREATE TABLE lore(
+  id SERIAL PRIMARY KEY,
+  lore_text TEXT NOT NULL,
+  about_chunk TEXT REFERENCES chunks(name)
+);
+
+-- "Data"/"Information"
+CREATE TABLE belongings(
+  id SERIAL PRIMARY KEY,
+  content TEXT,
+  owner TEXT REFERENCES chunks(name)
+);
