fa3c95498e3fa20f0e03f382fe4096f5045cf745
Ameer Arsala <ameer.arsala03@gmail.com>
2024-05-09 02:58:28 -0700

    funny message

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>In progress...</h1>
+<p>Visit <a href="https://ran.anemo.ai">RAN</a> because I said so</p>
