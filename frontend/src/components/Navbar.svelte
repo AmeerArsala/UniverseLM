7fcccaf9a0c680a79d27fe4511400b2643f45aef
Ameer Arsala <ameer.arsala03@gmail.com>
2024-05-20 05:42:47 -0700

    migration to astro basically complete

---
+++ frontend/src/components/Navbar.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+	import { NavbarMenu, MenuItem, ProductItem } from "./ui/NavbarMenu";
+  import HoveredLink from "./ui/NavbarMenu/HoveredLink.svelte";
+
+	let active: string | null = null;
+</script>
+
+<div class="relative flex w-full items-center justify-center">
+	<div class={'fixed inset-x-0 top-12 z-50 mx-auto max-w-2xl'}>
+		<NavbarMenu>
+			<MenuItem {active} item="Services">
+				<div class="flex flex-col space-y-4 text-sm">
+					<HoveredLink href="#">Web Development</HoveredLink>
+					<HoveredLink href="#">Interface Design</HoveredLink>
+					<HoveredLink href="#">Search Engine Optimization</HoveredLink>
+					<HoveredLink href="#">Branding</HoveredLink>
+				</div>
+			</MenuItem>
+			<MenuItem {active} item="Products">
+				<div class="  grid grid-cols-2 gap-10 p-4 text-sm">
+					<ProductItem
+						title="Algochurn"
+						href="#"
+						src="https://images.pexels.com/photos/1031659/pexels-photo-1031659.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+						description="Prepare for tech interviews like never before."
+					/>
+					<ProductItem
+						title="Tailwind Master Kit"
+						href="#"
+						src="https://images.pexels.com/photos/733071/pexels-photo-733071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+						description="Production ready Tailwind css components for your next project"
+					/>
+					<ProductItem
+						title="Moonbeam"
+						href="#"
+						src="https://images.pexels.com/photos/730424/pexels-photo-730424.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+						description="Never write from scratch again. Go from idea to blog in minutes."
+					/>
+					<ProductItem
+						title="Rogue"
+						href="#"
+						src="https://images.pexels.com/photos/745243/pexels-photo-745243.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+						description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
+					/>
+				</div>
+			</MenuItem>
+			<MenuItem {active} item="Pricing">
+				<div class="flex flex-col space-y-4 text-sm">
+					<HoveredLink href="#">Hobby</HoveredLink>
+					<HoveredLink href="#">Individual</HoveredLink>
+					<HoveredLink href="#">Team</HoveredLink>
+					<HoveredLink href="#">Enterprise</HoveredLink>
+				</div>
+			</MenuItem>
+		</NavbarMenu>
+	</div>
+</div>
---
+++ frontend/src/components/ui/NavbarMenu/NavbarMenu.svelte
@@ -0,0 +1,5 @@
+<nav
+	class="boder relative flex justify-center space-x-4 rounded-full bg-black px-8 py-6 shadow-input dark:border-white/[0.2]"
+>
+	<slot />
+</nav>
