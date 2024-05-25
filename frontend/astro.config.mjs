19081d635e5896c5e388611f2292162313b3a98c
Ameer Arsala <ameer.arsala03@gmail.com>
2024-05-25 08:37:36 -0700

    added markdown pages

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,15 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
-
 import node from "@astrojs/node";
 import vercel from "@astrojs/vercel/serverless";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false
-  }), svelte(), react()],
+  }), svelte(), react(), mdx()],
   output: "hybrid",
   adapter: vercel()
 });
