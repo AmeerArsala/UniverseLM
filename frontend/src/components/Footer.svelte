30e4e1ecf36cf44a88951dfec674ca654b3ce754
Ameer Arsala <ameer.arsala03@gmail.com>
2024-05-27 08:32:15 -0700

    thug shaker

--- frontend/src/components/Footer.svelte
+++ frontend/src/components/Footer.svelte
@@ -12,7 +12,7 @@
       </div>
       <div>
         <a href="/careers">Careers</a>
-        <a href="#">Support Us</a>
+        <a href="https://ko-fi.com/ameerarsala", target="_blank">Support Us</a>
         <a href="https://github.com/AmeerArsala/UniverseLM" target="_blank">GitHub</a>
         <a href="https://www.linkedin.com/in/ameer-arsala/" target="_blank"
           >LinkedIn</a
