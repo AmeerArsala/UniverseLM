68f3fe7a8bfe80100d6d52a71b962cc32b01587f
Ameer Arsala <ameer.arsala03@gmail.com>
2024-06-04 17:41:07 -0700

    boy, i sure love debugging prod part 3.11

--- frontend/src/data/stores.ts
+++ frontend/src/data/stores.ts
@@ -2,21 +2,22 @@ import { atom, map } from "nanostores";
 import { persistentAtom, persistentMap } from "@nanostores/persistent";
 
 // Web stores
-const numGitHubStars = persistentAtom("num_github_stars", "-1");
+const numGitHubStars = persistentAtom("num_github_stars");
 
 export const githubStars = {
   getNum: (): number => {
     return parseInt(numGitHubStars.get());
   },
-  setNum: (stars: number) => { numGitHubStars.set(stars.toString()); }
+  setNum: (stars: number) => {
+    numGitHubStars.set(stars.toString());
+  }
 };
-
 
 export const isAuthenticated = atom(false);
 export const shouldCheckAuthentication = atom(true);
 
 // App Stores
-export const userID = persistentAtom("user_id", null);
+export const userID = persistentAtom("user_id"); //, null);
 
 // Core stores
 export const community = atom({id: -1, name: ""});
